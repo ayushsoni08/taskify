7c3a8bb75d988c185fbcadb4ec193664c7975bd0
Ayush Soni <ayushsoni0718@gmail.com>
2024-07-28 23:02:59 +0530

    added organizations and protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,28 @@
-// import { clerkMiddleware } from "@clerk/nextjs/server";
-
-// export default clerkMiddleware();
-
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//     // Always run for API routes
-//     '/(api|trpc)(.*)',
-//   ],
-// };
-
+import { RedirectToSignIn } from '@clerk/nextjs';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']);
 
 export default clerkMiddleware((auth, request) => {
   if(!isPublicRoute(request)) {
     auth().protect();
+  }
+
+  if(auth().userId && isPublicRoute(request)) {
+    let path = '/select-org';
+
+    if(auth().orgId) {
+      path = `/organization/${auth().orgId}`
+    }
+
+    const orgSelection = new URL(path, request.url);
+    return NextResponse.redirect(orgSelection);
+  }
+
+  if(auth().userId && !auth().orgId && request.nextUrl.pathname !== '/select-org') {
+    const orgSelection = new URL("/select-org", request.url);
+    return NextResponse.redirect(orgSelection);
   }
 });
 
