d7cefaafeb51786f60e00762d004cdf05e705f92
Ayush Soni <ayushsoni0718@gmail.com>
2024-07-30 10:14:04 +0530

    added useAction abstraction

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,18 +1,30 @@
 "use client";
 
-import { create } from "@/actions/create-board";
-import { useFormState } from "react-dom";
 import { FormInput } from "./form-input";
 import { FormButton } from "./form-button";
+import { useAction } from "@/hooks/use-action";
+import { createBoard } from "@/actions/create-board";
 
 export const Form = () => {
 
-    const initialState = { message: "", errors: {} };
-    const [state, dispatch] = useFormState(create, initialState);
+    const {execute, fieldErrors} = useAction(createBoard, {
+        onSuccess: (data) => {
+            console.log(data, "SUCCESS!");
+        },
+        onError: (error) => {
+            console.error(error);
+            
+        }
+    });
+
+    const onSubmit = (formData: FormData) => {
+        const title = formData.get('title') as string;
+        execute({title});
+    }
 
     return (
-        <form action={dispatch}>
-            <FormInput errors={state?.errors} />
+        <form action={onSubmit}>
+            <FormInput errors={fieldErrors} />
             <FormButton />
         </form>
     );
