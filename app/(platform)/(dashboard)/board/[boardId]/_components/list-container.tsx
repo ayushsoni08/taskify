0e4a63634a8068316f91a5e31afe9fefdd0786be
Ayush Soni <ayushsoni0718@gmail.com>
2024-07-31 18:03:08 +0530

    added Drag n Drop functionality

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -5,33 +5,160 @@ import { ListWithCards } from "@/types";
 import { ListForm } from "./list-form";
 import { useEffect, useState } from "react";
 import { ListItem } from "./list-item";
+import { useAction } from "@/hooks/use-action";
+import { updateListOrder } from "@/actions/update-list-order";
+import { toast } from "sonner";
+import { updateCardOrder } from "@/actions/update-card-order";
 
 interface ListContainerProps {
     data: ListWithCards[];
     boardId: string;
-};
+}
 
-export const ListContainer = ({data, boardId}: ListContainerProps) => {
-    
-    const[orderedData, setOrderedData] = useState(data);
+function reorder<T>(list: T[], startIndex: number, endIndex: number) {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+}
+
+export const ListContainer = ({ data, boardId }: ListContainerProps) => {
+    const [orderedData, setOrderedData] = useState(data);
+
+    const { execute: executeUpdateListOrder } = useAction(updateListOrder, {
+        onSuccess: (data) => {
+            toast.success("List reordered");
+        },
+        onError: (error) => {
+            toast.error(error);
+        },
+    });
+
+    const { execute: executeUpdateCardOrder } = useAction(updateCardOrder, {
+        onSuccess: (data) => {
+            toast.success("Card reordered");
+        },
+        onError: (error) => {
+            toast.error(error);
+        },
+    });
 
     useEffect(() => {
         setOrderedData(data);
     }, [data]);
 
+    const onDragEnd = (result: any) => {
+        const { destination, source, type } = result;
+
+        // if no destination
+        if (!destination) return;
+
+        // if dropped in the same position
+        if (
+            destination.droppableId === source.droppableId &&
+            destination.index === source.index
+        )
+            return;
+
+        // user moves a list
+        if (type === "list") {
+            const items = reorder(orderedData, source.index, destination.index).map(
+                (item, index) => ({ ...item, order: index })
+            );
+
+            setOrderedData(items);
+
+            // update list order in server
+            executeUpdateListOrder({ items, boardId });
+        }
+
+        // user moves a card
+        if (type === "card") {
+            let newOrderedData = [...orderedData];
+
+            // source and destination list
+            const sourceList = newOrderedData.find(
+                (list) => list.id === source.droppableId
+            );
+
+            const destinationList = newOrderedData.find(
+                (list) => list.id === destination.droppableId
+            );
+
+            if (!sourceList || !destinationList) return;
+
+            // check if cards exists on the source list
+            if (!sourceList.cards) sourceList.cards = [];
+
+            // check if cards exists on the destination list
+            if (!destinationList.cards) destinationList.cards = [];
+
+            // moving the card in the same list
+            if (source.droppableId === destination.droppableId) {
+                const reorderedCards = reorder(
+                    sourceList.cards,
+                    source.index,
+                    destination.index
+                );
+
+                reorderedCards.forEach((card, i) => {
+                    card.order = i;
+                });
+
+                sourceList.cards = reorderedCards;
+                setOrderedData(newOrderedData);
+
+                executeUpdateCardOrder({ boardId, items: reorderedCards });
+            }
+
+            // user moves card to another list
+            else {
+                // remove card from the source list
+                const [movedCard] = sourceList.cards.splice(source.index, 1);
+
+                // assign the new list id to the moved card
+                movedCard.listId = destination.droppableId;
+
+                // add new card to the destination list
+                destinationList.cards.splice(destination.index, 0, movedCard);
+
+                sourceList.cards.forEach((card, i) => {
+                    card.order = i;
+                });
+
+                // update the order for each card in destination list
+                destinationList.cards.forEach((card, i) => {
+                    card.order = i;
+                });
+
+                setOrderedData(newOrderedData);
+
+                executeUpdateCardOrder({ boardId: boardId, items: destinationList.cards });
+            }
+        }
+    };
+
     return (
-        <DragDropContext>
-            <ol className="flex gap-x-3 h-full">
-                {orderedData.map((list, i) => (
-                    <ListItem 
-                        key={list.id}
-                        index={i}
-                        data={list}
-                    />
-                ))}
-                <ListForm />
-                <div className="flex-shrink-0 w-1" />
-            </ol>
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="lists" type="list" direction="horizontal">
+                {(provided) => (
+                    <ol
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                        className="flex gap-x-3 h-full"
+                    >
+                        {orderedData.map((list, i) => (
+                            <ListItem key={list.id} index={i} data={list} />
+                        ))}
+
+                        {provided.placeholder}
+
+                        <ListForm />
+                        <div aria-hidden className="flex-shrink-0 w-1" />
+                    </ol>
+                )}
+            </Droppable>
         </DragDropContext>
-    )
-}
+    );
+};
