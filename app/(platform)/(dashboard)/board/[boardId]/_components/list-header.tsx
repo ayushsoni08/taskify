a6de2d1168d97edf413d12720feeff547e455555
Ayush Soni <ayushsoni0718@gmail.com>
2024-07-31 13:26:02 +0530

    added list options component and server actions

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -8,12 +8,14 @@ import { List } from "@prisma/client";
 import { FormInput } from "@/components/form/form-input";
 import { useAction } from "@/hooks/use-action";
 import { updateList } from "@/actions/update-list";
+import { ListOptions } from "./list-options";
 
 type ListHeaderProps = {
   data: List;
+  onAddCard: ()=> void;
 };
 
-export const ListHeader = ({ data }: ListHeaderProps) => {
+export const ListHeader = ({ data, onAddCard }: ListHeaderProps) => {
   const [title, setTitle] = useState(data.title);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -107,6 +109,8 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
           {data.title}
         </div>
       )}
+
+    <ListOptions data={data} onAddCard={onAddCard} />
     </div>
   );
 };
